fdc4f453a6b56bbd2318912b95d57d27fa1e85ce
Iaroslav <yarik.vdovychenko@gmail.com>
2017-05-15 07:58:55 +0300

    add func

--- src/app/Quiz.jsx
+++ src/app/Quiz.jsx
@@ -5,10 +5,20 @@ export default class Quiz extends React.Component {
         super(props);
         this.state = {
             emptyCont: false,
-
+            isCheck: [true, false, false, false]
         }
 
     };
+
+    radioCheck(e) {
+        let current = e.target.id;
+        let arr = [false, false, false, false];
+        arr[current] = true;
+
+        this.setState({isCheck:[false, false, false, false]});
+
+        this.setState({isCheck: arr});
+    }
 
 
 
@@ -25,6 +35,18 @@ export default class Quiz extends React.Component {
                 <div className={showTestBlock}>
                     <div>
                         {this.props.quest}
+                    </div>
+                    <div>
+                        <input type="radio" name="0"  id={0} checked={this.state.isCheck[0]}  onChange={this.radioCheck}/>
+                        <label for="0">{this.props.answers[0]}</label> <br/>
+                        <input type="radio" name="1"  id={1} checked={this.state.isCheck[1]}  onChange={this.radioCheck}/>
+                        <label for="1">{this.props.answers[1]}</label> <br/>
+                        <input type="radio" name="2"  id={2} checked={this.state.isCheck[2]}  onChange={this.radioCheck}/>
+                        <label for="2">{this.props.answers[2]}</label> <br/>
+                        <input type="radio" name="3"  id={3} checked={this.state.isCheck[3]}  onChange={this.radioCheck}/>
+                        <label for="3">{this.props.answers[3]}</label> <br/>
+                        <div></div>
+                        <button>Send</button>
                     </div>
 
                 </div>
